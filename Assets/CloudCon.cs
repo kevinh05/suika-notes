6c7174e10db883b70f22e4f78d7ad9a53a7a8372
sdoshi4 <shaandoshi4@gmail.com>
2023-11-18 23:22:09 -0500

    add note prefabs and intro game

--- Assets/CloudCon.cs
+++ Assets/CloudCon.cs
@@ -1,9 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml;
+using Unity.VisualScripting;
 using UnityEngine;
-
+    
 public class CloudCon : MonoBehaviour
 {
+    public Transform[] fruitObj;
+    static public string spawnedYet = "n";
+    static public Vector2 cloudxPos;
+    public int numOfSpawnableFruits = 2;
+    static public Vector2 spawnPos;
+    static public string newFruit = "n";
+    static public int whichFruit = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,18 +22,68 @@ public class CloudCon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        spawnFruit();
+        replaceFruit();
+
         if (Input.GetKey("a"))
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(-2, 0);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-4, 0);
         }
         if (Input.GetKey("d"))
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(2, 0);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(4, 0);
         }
         if ((!Input.GetKey("a")) && (!Input.GetKey("d")))
         {
             GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
         }
 
+        if (GetComponent<Rigidbody2D>().velocity.x < 0 && transform.position.x < -2.05)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+        }
+
+        if (GetComponent<Rigidbody2D>().velocity.x > 0 && transform.position.x > 2.0)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+        }
+
+        cloudxPos = transform.position;
+
+        if (Input.GetKeyDown("space") && spawnedYet == "y")
+        {
+            spawnedYet = "n";   
+        }
+    }
+
+    void spawnFruit()
+    {
+        if (spawnedYet == "n")
+        {
+            StartCoroutine(spawnTimer());
+            //Instantiate(fruitObj[Random.Range(0, numOfSpawnableFruits)], transform.position, fruitObj[0].rotation);
+            spawnedYet = "w";
+        }
+    }
+
+    void replaceFruit()
+    {
+        if (newFruit == "y")
+        {
+            newFruit = "n";
+            if (whichFruit + 1 < fruitObj.Length)
+            {
+                Instantiate(fruitObj[whichFruit+1], spawnPos, fruitObj[0].rotation);
+            } else
+            {
+                Instantiate(fruitObj[whichFruit], spawnPos, fruitObj[0].rotation);
+            }
+        }
+    }
+    IEnumerator spawnTimer()
+    {
+        yield return new WaitForSeconds(.5f);
+        Instantiate(fruitObj[Random.Range(0, numOfSpawnableFruits)], transform.position, fruitObj[0].rotation);
+        spawnedYet = "y";
     }
 }
---
+++ Assets/FruitCon.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FruitCon : MonoBehaviour
+{
+    private string inTheCloud = "y";
+    private string timeToCheck = "n";
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (transform.position.y < 5)
+        {
+            inTheCloud = "n";
+            GetComponent<Rigidbody2D>().gravityScale = 1;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inTheCloud == "y")
+        {
+            GetComponent<Transform>().position = CloudCon.cloudxPos;
+            GetComponent<Rigidbody2D>().gravityScale = 0;
+        }
+
+        if (Input.GetKeyDown("space"))
+        {
+            GetComponent<Rigidbody2D>().gravityScale = 1;
+            inTheCloud = "n";
+            StartCoroutine(checkGameOver());
+            //CloudCon.spawnedYet = "n";
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == gameObject.tag)
+        {
+            CloudCon.spawnPos.x = (this.transform.position.x + collision.collider.transform.position.x) / 2;
+            CloudCon.spawnPos.y = (this.transform.position.y + collision.collider.transform.position.y) / 2;
+            CloudCon.newFruit = "y";
+            CloudCon.whichFruit = int.Parse(gameObject.tag);
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.name=="limit" && timeToCheck == "y") 
+        {
+            Debug.Log("game over");
+
+            // Scene Manager.LoadScene
+        }
+    }
+
+    IEnumerator checkGameOver()
+    {
+        yield return new WaitForSeconds(.75f);
+        timeToCheck = "y";
+    }
+}
